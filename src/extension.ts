54d9da0d55f7065b39851438660fc9de551e9d6f
Luiz Fernando Silva <luizinho_mack@yahoo.com.br>
2022-01-20 11:57:28 -0300

    Cleaning up extension code

--- src/extension.ts
+++ src/extension.ts
@@ -1,28 +1,13 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import path = require('path');
 import * as vscode from 'vscode';
 import * as fs from 'fs';
 import { findSwiftPackage, swiftPackageManifestForFile } from './swiftPackageFinder';
 import { proposeTestFiles } from './testFileGeneration';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "swifttestgen" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('swifttestgen.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from SwiftTestGen!');
-	});
-	context.subscriptions.push(disposable);
+	let disposable: vscode.Disposable;
 
 	disposable = vscode.commands.registerCommand('swifttestgen.generateTestFile', async (_, fileUris: vscode.Uri[]) => {
 		const packagePaths = await Promise.all(fileUris.map((fileUri) => {
@@ -44,14 +29,14 @@ export function activate(context: vscode.ExtensionContext) {
 
 		const packageManifestPath = filteredPackagePaths[0];
 		const packagePath = vscode.Uri.joinPath(packageManifestPath, "..");
-		
+
 		try {
 			const pkg = await swiftPackageManifestForFile(packageManifestPath);
-			
+
 			const testFiles =
 				proposeTestFiles(fileUris, packagePath, pkg)
-				.filter(testFile => !fs.existsSync(testFile.path.fsPath));
-			
+					.filter(testFile => !fs.existsSync(testFile.path.fsPath));
+
 			const wsEdit = new vscode.WorkspaceEdit();
 
 			testFiles.forEach(file => {
@@ -66,6 +51,3 @@ export function activate(context: vscode.ExtensionContext) {
 	});
 	context.subscriptions.push(disposable);
 }
-
-// this method is called when your extension is deactivated
-export function deactivate() {}
